6ec25b920d082ab8edae4feafec1a6f8b384d1cd
ShwetaKadam-Git <108974418+ShwetaKadam-Git@users.noreply.github.com>
2025-04-01 21:18:51 +0530

    Update swiatek_app.py

--- swiatek_app.py
+++ swiatek_app.py
@@ -121,6 +121,9 @@ elif section == "Data Visualizations":
     if chart_type == "Feature Correlation Heatmap":
         st.subheader("Feature Correlation Heatmap")
         fig, ax = plt.subplots(figsize=(10, 6))
+        st.write(df.head())  # Check if df is loaded
+        st.write(df_numeric.head())  # Check if df_numeric exists
+
         sns.heatmap(df_numeric.corr(), annot=True, cmap="coolwarm", fmt=".2f", ax=ax)
         st.pyplot(fig)
 
